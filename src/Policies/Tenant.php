281ebec06940dd2afe3d9c60291aa7ee777b38d8
Braden Keith <bkeith@romegadigital.com>
2019-01-18 12:00:54 -0500

    Policy to block everything by default

--- src/Policies/Tenant.php
+++ src/Policies/Tenant.php
@@ -3,52 +3,13 @@
 namespace RomegaDigital\MultitenancyNovaTool\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Support\Facades\Cache;
 
 class Tenant
 {
     use HandlesAuthorization;
 
     /**
-     * Checks if user is allowed to see the resource.
-     *
-     * @param \Illuminate\Database\Eloquent\Model $user
-     * @return void
+     * Allow nothing by default; gate within multitenancy package
+     * will allow users who can 'access admin'.
      */
-    public function viewAny($user)
-    {
-        return Cache::remember('tenant-policy:' . $user->id, 10, function () use ($user) {
-            return $user->can('access admin');
-        });
-    }
-
-    public function view()
-    {
-        return true;
-    }
-
-    public function create()
-    {
-        return true;
-    }
-
-    public function update()
-    {
-        return true;
-    }
-
-    public function delete()
-    {
-        return true;
-    }
-
-    public function restore()
-    {
-        return true;
-    }
-
-    public function forceDelete()
-    {
-        return true;
-    }
 }
